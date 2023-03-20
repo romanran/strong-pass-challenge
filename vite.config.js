ff20d34c214d113c9fc12532ffe8c3ea5f70c1f8
Roman <romanranniew@gmail.com>
2023-03-20 17:57:47 +0100

    Change configs

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,24 @@
-import { fileURLToPath, URL } from "node:url";
+import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import svgLoader from 'vite-svg-loader'
+import postcssNesting from 'postcss-nesting'
+
+const config = {
+    svgLoader: { defaultImport: 'raw' },
+    css: {
+        postcss: { plugins: [postcssNesting()] }
+    }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    plugins: [vue(), svgLoader(config.svgLoader)],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     },
-  },
-});
+    css: config.css
+})
